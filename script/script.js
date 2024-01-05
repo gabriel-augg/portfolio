0ce097576d8607739cd789287d0ad5e7512c655a
Gabriel <gabrielgapseu@gmail.com>
2024-01-04 21:53:16 -0300

    Feat: adicionar e deletar

--- script/script.js
+++ script/script.js
@@ -8,3 +8,10 @@ function aparecerMenu(){
         document.getElementById('icone').src = 'assets/icons/close-icon.svg'        
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt("#texto", {
+        speed: 200,
+        loop: true
+    }).type('Full-Stack', {delay: 1000}).delete(10).type('Front-End', {delay: 1000}).delete(9).type('Back-End', {delay: 1000}).delete(8).go()
+})
