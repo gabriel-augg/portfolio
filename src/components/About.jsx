d0835963c0ca8d3f09cd469952c929765b0f25fd
Gabriel <gabrielgapseu@gmail.com>
2024-04-27 09:30:13 -0300

    Feat: responsivity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
             <Title title="Sobre mim" />
             <div className="flex sm:flex-col lg:flex-row items-center lg:gap-0 sm:gap-10 justify-center mt-20">
                 <aside className='flex-1 flex justify-center items-center'>
-                    <img src={illustration} className='rounded-2xl shadow-2xl' width={250} alt="ilustration" />
+                    <img src={illustration} className='rounded-2xl shadow-2xl' width={280} alt="ilustration" />
                 </aside>
                 <article className='flex-1 lg:text-start sm:text-center'>
                     <h1 className='text-2xl text-primary-dark font-bold'>Quem sou eu?</h1>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 const Home = () => {
     return (
-        <section id="home" className="flex items-center h-screen bg-secondary-dark p-16">
+        <section id="home" className="flex items-center h-screen bg-secondary-dark text-white p-16">
             <div>
                 <h1 className="text-5xl font-bold text-primary-dark">Desenvolvedor Fullstack</h1>
-                <p className="text-white text-2xl mt-6 ">Aqui você vai ver um pouquinho de quem eu sou e alguns dos meus projetos.</p>
+                <p className="text-2xl my-6 ">Aqui você vai ver um pouquinho de quem eu sou e alguns dos meus projetos.</p>
+                <a className="bg-primary-dark p-1 px-5 rounded-lg font-bold">Projetos</a>
             </div>
         </section>
     )
