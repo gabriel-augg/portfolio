2861d7f9cbe2734c1df162c39f94a0866c5087cd
Gabriel <gabrielgapseu@gmail.com>
2024-04-26 22:38:08 -0300

    Feat: medialink component

---
+++ src/components/MediaLink.jsx
@@ -0,0 +1,8 @@
+const MediaLink = ({ to, children }) => {
+  return (
+    <a href={to} className="text-light bg-primary-light hover:bg-primary-dark font-bold flex items-center gap-2 p-1 px-2 rounded-md"> {children}
+    </a>
+  )
+}
+
+export default MediaLink;
