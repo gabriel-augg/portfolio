2e19d9aef4fa4c06b7bda68692edb255f63b5887
Gabriel <gabrielgapseu@gmail.com>
2024-04-26 22:32:40 -0300

    Feat: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import Logo from "./Logo";
+
+const Header = () => {
+  return (
+    <header className="fixed w-full bg-secondary-dark top-0">
+      <nav className="flex justify-between p-4 px-12">
+        <Logo />
+        <ul className="flex items-center gap-5 text-light-light font-bold">
+            <li>
+                <a href="#home">Home</a>
+            </li>
+            <li>
+                <a href="#about">Sobre</a>
+            </li>
+            <li>
+                <a href="#services">Projetos</a>
+            </li>
+            <li>
+                <a href="#contact">Contato</a>
+            </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
