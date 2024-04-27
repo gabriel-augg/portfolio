ac6ffa0b38986e149f3d89cd1ceb5ccaf5346279
Gabriel <gabrielgapseu@gmail.com>
2024-04-26 22:33:01 -0300

    Feat: homepage section

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <section id="home" className="flex items-center h-screen bg-secondary-dark p-16">
+            <div>
+                <h1 className="text-5xl font-bold text-primary-dark">Desenvolvedor Fullstack</h1>
+                <p className="text-white text-2xl mt-6 ">Aqui você vai ver um pouquinho de quem eu sou e alguns dos meus projetos.</p>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
