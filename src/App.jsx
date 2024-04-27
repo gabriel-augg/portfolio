ab13047a8bb798a1ad51e6f24417179689266ed3
Gabriel <gabrielgapseu@gmail.com>
2024-04-27 15:30:43 -0300

    Feat: footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
 import Projects from './components/Projects'
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   )
 }
