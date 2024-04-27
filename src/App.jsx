1282cf8984ea9ee7a11964ba1e3dd92b8f0bed7c
Gabriel <gabrielgapseu@gmail.com>
2024-04-27 09:35:58 -0300

    Refactor: alteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Header from './components/Header'
+import Home from './components/Home'
+import Projects from './components/Projects'
 
 
 function App() {
 
   return (
     <>
+      <Header />
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </>
   )
 }
---
+++ src/helpers/projects.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        title: "Wonder",
+        description: "Wonder é uma rede social focada em permitir que usuários publiquem mensagens de texto, proporcionando uma plataforma aberta para liberdade de expressão.",
+        label: "Front-End",
+        stacks: ["React.js", "ContextAPI", "Axios", "Zod"],
+        link: "https://github.com/gabriel-augg/wonder"
+    },
+    {
+        title: "Wonder-API RESTful",
+        description: "API desenvolvida seguindo os padrões REST com o intuito de ser integrada ao projeto Wonder.",
+        label: "Back-End",
+        stacks: ["Node.js", "Express", "MySQL", "JWT", "Bcrypt", "MVC"],
+        link: "https://github.com/gabriel-augg/wonder-api"
+    }
+]
