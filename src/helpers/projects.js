0d72a11c45b53b7ba125a95c15c468afec2f3e7d
Gabriel <gabrielgapseu@gmail.com>
2024-04-29 20:33:29 -0300

    Style: change on style

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -3,21 +3,21 @@ export default [
         title: "Wonder",
         description: "Wonder é uma rede social focada em permitir que usuários publiquem mensagens de texto, proporcionando uma plataforma aberta para liberdade de expressão.",
         label: "Front",
-        stacks: ["React.js", "ContextAPI", "Axios", "Zod"],
+        stacks: ["JavaScript", "React.js", "ContextAPI", "Axios", "Zod"],
         link: "https://github.com/gabriel-augg/wonder"
     },
     {
         title: "Wonder-API RESTful",
         description: "API desenvolvida seguindo os padrões REST com o intuito de ser integrada ao projeto Wonder.",
         label: "API",
-        stacks: ["Node.js", "Express", "MySQL", "JWT", "Bcrypt", "MVC"],
+        stacks: ["JavaScript","Node.js", "Express", "MySQL", "JWT", "Bcrypt", "MVC"],
         link: "https://github.com/gabriel-augg/wonder-api"
     },
     {
         title: "Portifólio",
         description: "Portifólio desenvolvido com o propósito de mostrar quem sou eu, tecnologias, projetos pessoais e contato.",
         label: "Front",
-        stacks: ["React.js", "TailwindCSS", "Typeit"],
+        stacks: ["JavaScript", "React.js", "TailwindCSS", "Typeit"],
         link: "https://github.com/gabriel-augg/portfolio"
     }
 ]
