6b9ac5e19624640e431c4680bdb6196d0fbf06fd
Gabriel <gabrielgapseu@gmail.com>
2024-05-15 19:27:04 -0300

    Feat: add new projects

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -2,21 +2,35 @@ export default [
     {
         title: "Wonder",
         description: "Wonder é uma rede social focada em permitir que usuários publiquem mensagens de texto, proporcionando uma plataforma aberta para liberdade de expressão.",
-        label: "Front",
+        label: "Client",
         stacks: ["JavaScript", "React.js", "ContextAPI", "Axios", "Zod"],
         link: "https://github.com/gabriel-augg/wonder"
     },
     {
         title: "Wonder-API RESTful",
         description: "API desenvolvida seguindo os padrões REST com o intuito de ser integrada ao projeto Wonder.",
-        label: "API",
+        label: "Server",
         stacks: ["JavaScript","Node.js", "Express", "MySQL", "JWT", "Bcrypt", "MVC"],
         link: "https://github.com/gabriel-augg/wonder-api"
     },
     {
+        title: "Minhas notas",
+        description: "Minhas notas é um aplicativo de gerenciamento de notas que nasceu com o objetivo de organizar ideias 💡",
+        label: "Client",
+        stacks: ["JavaScript", "React.js", "ContextAPI", "Axios", "Toastify", "TailwindCSS", "DaisyUI"],
+        link: "https://github.com/gabriel-augg/minhas-notas"
+    },
+    {
+        title: "Minhas notas API RESTful",
+        description: "API desenvolvida seguindo os padrões REST com o intuito de ser integrada ao projeto Minhas notas.",
+        label: "Server",
+        stacks: ["JavaScript","Node.js", "Express", "MySQL", "JWT", "Bcrypt", "MVC"],
+        link: "https://github.com/gabriel-augg/minhas-notas-api"
+    },
+    {
         title: "Portifólio",
         description: "Portifólio desenvolvido com o propósito de mostrar quem sou eu, tecnologias, projetos pessoais e contato.",
-        label: "Front",
+        label: "Client",
         stacks: ["JavaScript", "React.js", "TailwindCSS", "Typeit"],
         link: "https://github.com/gabriel-augg/portfolio"
     }
